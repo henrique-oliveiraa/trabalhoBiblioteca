cda2815cb3fd8cb88445ff45d7d9431813a5fb28
henrique-oliveiraa <148977691+henrique-oliveiraa@users.noreply.github.com>
2024-03-17 10:53:47 -0300

    Update Biblioteca.ts

--- Biblioteca.ts
+++ Biblioteca.ts
@@ -3,22 +3,26 @@ export class Biblioteca {
 
 //Esta função deve listar TODOS os livros disponíveis na biblioteca
 listaLivrosDisponíveis(){
-
+let livros_disponiveis: livros_disponiveis[] = ["Homem de Ferro, Carros"];
+  
 }
 
 //Esta função deve listar TODOS os livros emprestados na biblioteca
 listaLivrosEmprestados(){
-
+let livros_emprestados: livros_emprestados[] = ["Estrelas"];
+  
 }
 
 //Esta função deve listar TODOS os livros atrasados na biblioteca
 listaLivrosAtrasados(){
-
+let livros_atrasados: livros_atrasados[] = ["Mulher Maravilha"];
+  
 }
 
 //Lista todos os livros que tem um determinado genero
 listaPorGenero(){
-
+let genero: genero [] = ["Homem de Ferro", "Mulher Maravilha", "Carros", "Estrelas", "Pequeno Principe"];
+  
 }
 
 //Esta função deve cadastrar um novo usuário
